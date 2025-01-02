0f972b2f9f5209105805eb4700fbbc05d7cd2321
dhanjiRajput <kidechadhanji@gmail.com>
2025-01-02 15:37:11 +0530

    updated

--- like_component/src/LikeComponent.jsx
+++ like_component/src/LikeComponent.jsx
@@ -2,23 +2,25 @@ import { useState } from "react";
 
 const LikeComponent = () => {
   const [like, setlike] = useState(false);
+  const [count, setcount] = useState(0);
 
   const handleLike = () => {
     setlike(!like);
+    if(like==false){
+        setcount(count+1);
+    }
   };
 
   return (
     <div>
       <button onClick={handleLike}>
-        {like ? (
-          <i
-            className="fa-solid fa-heart"
-            style={{ fontSize: "150px", color: "red",borderRadius:"50px" }}
-          ></i>
-        ) : (
-          <i className="fa-regular fa-heart" style={{ fontSize: "150px" }}></i>
-        )}
+        {
+            like
+                ? <i className="fa-solid fa-heart" style={{ fontSize: "150px", color: "red",borderRadius:"50px" }}></i>
+                : <i className="fa-regular fa-heart" style={{ fontSize: "150px" }} ></i>
+        }
       </button>
+      <h1>Total Likes : {count}</h1>
     </div>
   );
 };
